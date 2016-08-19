5e8dfad2cb9bdd0f1b30feeec23b510e986346e5
Dan Guest <dguest@cern.ch>
2016-08-19 14:56:47 +0200

    make private functions in hadd public (so we can do other stuff to them)

--- python/ndhist/hadd.py
+++ python/ndhist/hadd.py
@@ -1,7 +1,7 @@
 from ndhist.hist import Hist, is_h5_hist
 from h5py import File
 
-def _add_tree(base, new):
+def add_tree(base, new):
     for name, sub in new.items():
         # if sub is a hist, add it to base
         if is_h5_hist(sub):
@@ -13,15 +13,15 @@ def _add_tree(base, new):
         else:
             if name not in base:
                 base[name] = {}
-            _add_tree(base[name], sub)
+            add_tree(base[name], sub)
 
-def _build_h5_tree(base_group, base_dic):
+def build_h5_tree(base_group, base_dic):
     for name, sub in base_dic.items():
         assert name not in base_group
         # if the sub is a dic, create a group
         if isinstance(sub, dict):
             subgroup = base_group.create_group(name)
-            _build_h5_tree(subgroup, sub)
+            build_h5_tree(subgroup, sub)
         # otherwise, assume it's a hist
         else:
             sub.write(base_group, name)
@@ -33,8 +33,8 @@ def hadd(out_file_name, file_itr, verbose=False):
         if verbose:
             print('adding {}'.format(fname))
         with File(fname, 'r') as infile:
-            _add_tree(htree, infile)
+            add_tree(htree, infile)
 
     # now write out
     with File(out_file_name, 'w') as outfile:
-        _build_h5_tree(outfile, htree)
+        build_h5_tree(outfile, htree)
